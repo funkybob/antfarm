2ceb1f9c21cec5eec77b92ad8ea3ddb3591266e1
funkybob <curtis@tinbrain.net>
2014-02-01 12:49:41 +1100

    Properly handle extra kwargs in urls Build URL instance more cleanly in dispatcher Add docs

--- antfarm/urls.py
+++ antfarm/urls.py
@@ -1,4 +1,20 @@
 
+'''
+Django-style URL dispatcher view.
+
+    App(root_url=url_dispatcher([
+        (r'^/$', views.index),
+        (re.compile(r'^/(?P<foo>\d+)/'), views.detail, {'bar': True}),
+    ])
+
+The view will be called with the request, and any matched _named_ groups.
+Extra kwargs can be passed as a 3rd positional argument.
+
+There is a namedtuple class defined called URL provided.  All patterns will be
+assembled as a URL instance, and their regex compiled.  If kwargs are not
+specified, they will default to {}.
+'''
+
 from collections import namedtuple
 
 import re
@@ -8,22 +24,31 @@ class KeepLooking(Exception):
     '''Used to tell a url_dispatcher to skip this pattern and keep looking.'''
     pass
 
-# XXX If the number of regexes grows, it may be worth pre-compiling
-class url(namedtuple('url', ('regex', 'view', 'kwargs',))
+URL = namedtuple('url', ('regex', 'view', 'kwargs',))
 
 class url_dispatcher(object):
     def __init__(self, patterns):
-        self.patterns = [
-            url(*p) if not isinstance(p, url) else p
-            for p in patterns
-        ]
+        self.patterns = map(self._make_url, patterns)
+
+    def _make_url(self, pattern):
+        '''Helper to ensure all patterns are url instances.'''
+        if not isinstance(pattern, URL):
+            if len(pattern) == 2:
+                pattern = list(pattern) + [{}]
+            pattern[0] = re.compile(pattern[0])
+            pattern = URL(*pattern)
+        # Ensure the regex is compiled
+        pattern.regex = re.compile(pattern.regex)
+        return pattern
 
     def __call__(self, request):
         for pattern in self.patterns:
             m = pattern.regex.match(request.path)
             if m:
+                kwargs = dict(pattern.kwargs)
+                kwargs.update(pattern.kwargs)
                 try:
-                    return pattern.view(request, **pattern.kwargs)
+                    return pattern.view(request, **kwargs)
                 except KeepLooking:
                     pass
 
