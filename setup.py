f435724c6c1928b48aa42d7f53e9f61d06febf89
Curtis Maloney <curtis@tinbrain.net>
2014-02-16 16:43:59 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     zip_safe=False,
     classifiers = [
         'Environment :: Web Environment',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
