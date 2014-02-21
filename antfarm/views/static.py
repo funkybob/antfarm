ab12d9e847448750067e798ba1b5a4238451dfee
funkybob <curtis@tinbrain.net>
2014-02-21 12:11:07 +1100

    Handle missing files gracefully in ServeStatic

--- antfarm/views/static.py
+++ antfarm/views/static.py
@@ -3,6 +3,8 @@
 Helper for serving static content.
 
 '''
+import os.path
+
 from antfarm import response
 import mimetypes
 
@@ -10,8 +12,8 @@ class ServeStatic(object):
     def __init__(self, root):
         self.root = root
 
-    def __call__(self, path):
-        full_path = os.path.absdir(os.path.join(self.root, path))
+    def __call__(self, request, path):
+        full_path = os.path.abspath(os.path.join(self.root, path))
         if not full_path.startswith(self.root):
             return response.NotFound()
 
@@ -19,6 +21,9 @@ class ServeStatic(object):
         content_type, encoding = mimetypes.guess_type(full_path)
         content_type = content_type or 'application/octet-stream'
 
-        return response.Response(iter(open(full_path, 'rb')),
-            content_type=content_type
-        )
+        try:
+            fin = open(full_path, 'rb')
+        except FileNotFoundError:
+            return response.NotFound()
+
+        return response.Response(iter(fin), content_type=content_type)
