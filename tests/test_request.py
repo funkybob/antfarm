0622af32125f03362cceab10f21d22036b76a3ec
Keryn Knight <keryn@kerynknight.com>
2014-02-22 13:07:53 +0000

    unit test the expectations for getting the querystring from a request.

--- tests/test_request.py
+++ tests/test_request.py
@@ -32,5 +32,23 @@ class RequestTest(TestCase):
         ))
         self.assertEqual(req.cookies['keebler'], "E=everybody; L=\"Loves\"; fudge=\012;")
 
+    def test_003_querystring(self):
+        req = request.Request(None, dict(BASE_ENVIRON,
+                              QUERY_STRING='a=1&b=something&c=01/01/1970'))
+        self.assertEqual(req.query_data,
+                         {'a': ['1'], 'c': ['01/01/1970'], 'b': ['something']})
+
+    def test_004_querystring_duplicate_keys(self):
+        req = request.Request(None, dict(BASE_ENVIRON,
+                              QUERY_STRING='a=1&b=2&a=3'))
+        self.assertEqual(req.query_data,
+                         {'a': ['1', '3'], 'b': ['2']})
+
+    def test_005_querystring_blanks(self):
+        req = request.Request(None, dict(BASE_ENVIRON,
+                              QUERY_STRING='a=&b=2&c='))
+        self.assertEqual(req.query_data,
+                         {'c': [''], 'b': ['2'], 'a': ['']})
+
 if __name__ == '__main__':
     main()
