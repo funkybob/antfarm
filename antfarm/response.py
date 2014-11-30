e7698f1d933b82de5b6779a9e067f02692f40d3d
Michael Manfre <mmanfre@gmail.com>
2014-11-29 20:51:30 -0500

    Update response.py

--- antfarm/response.py
+++ antfarm/response.py
@@ -46,7 +46,7 @@ STATUS_CODES = (
     (415, 'Unsupported Media Type'),
     (416, 'Requested Range Not Satisfiable'),
     (417, 'Expectation Failed'),
-    (418, 'I\'m a teapot'),
+    (418, "I'm a teapot"),
 
     (500, 'Internal Server Error'),
     (501, 'Not Implemented'),
