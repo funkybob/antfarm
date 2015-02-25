dc4a3e7cb1d3d76ea600e4ce2b1e751273993d93
funkybob <curtis@tinbrain.net>
2015-02-25 21:47:10 +1100

    Add Py3 to setup tags

--- setup.py
+++ setup.py
@@ -15,5 +15,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
     ],
 )
