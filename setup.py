40a7cc450eba09a280b7bc8f7c68a807b0177c62
funkybob <curtis@tinbrain.net>
2015-03-02 15:33:49 +1100

    Bump version for the same reason

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='antfarm',
-    version='0.0.2',
+    version='0.0.3',
     description='An ultra-light-weight web framework',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
